efd415855a9269044ac065abce6db7d9baa8ef1a
rzdannisa <rizda25.9c@gmail.com>
2018-01-09 11:43:37 +0700

    up

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IZI | Memudahkan Dimudahkan</title>
+	<link rel="stylesheet" href="asset/bootstrap/dist/css/bootstrap.min.css">
+	<script src="asset/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+
+<div class="container" style="width: 100%">
+	<div class="box-body">
+     <div class="row">
+
+      <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/4.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+	  <br>
+
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <div class="card-body">
+	      <h4 class="card-title">Bantu Biaya Operasi Kanker Mata Adik Try</h4>
+	      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	    <img class="card-img-bottom" src="pict/2.jpg" alt="Card image" style="width:100%">
+	  </div>
+	 </div>
+	 <br>
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/3.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+
+
+	</div>
+	<br>
+	<div class="row">
+
+      <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/3.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+	  <br>
+
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <div class="card-body">
+	      <h4 class="card-title">Bantu Biaya Operasi Kanker Mata Adik Try</h4>
+	      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	    <img class="card-img-bottom" src="pict/5.jpg" alt="Card image" style="width:100%">
+	  </div>
+	 </div>
+	 <br>
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/4.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+
+	</div>
+	<br>
+	<div class="row">
+
+      <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/5.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+	  <br>
+
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <div class="card-body">
+	      <h4 class="card-title">Bantu Biaya Operasi Kanker Mata Adik Try</h4>
+	      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	    <img class="card-img-bottom" src="pict/3.jpg" alt="Card image" style="width:100%">
+	  </div>
+	 </div>
+	 <br>
+
+	 <div class="col-xs-12 col-sm-4">
+	  <div class="card" style="width: 100%;margin-bottom: 10px;">
+	    <img class="card-img-top" src="pict/2.jpg" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
+	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	      <a href="#" class="btn btn-primary">See Detail</a>
+	    </div>
+	  </div>
+	  </div>
+
+	</div>
+
+</div>
+
+</body>
+</html>
