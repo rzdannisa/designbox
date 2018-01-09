6236213d3618e941d57b828342bd7d8169ff520b
rzdannisa <rizda25.9c@gmail.com>
2018-01-09 11:56:26 +0700

    update

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <head>
 	<title>IZI | Memudahkan Dimudahkan</title>
 	<link rel="stylesheet" href="asset/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="asset/img/css.css">
 	<script src="asset/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
  
@@ -14,16 +17,41 @@
 
       <div class="col-xs-12 col-sm-4">
 	  <div class="card" style="width: 100%;margin-bottom: 10px;">
-	    <img class="card-img-top" src="pict/4.jpg" alt="Card image" style="width:100%">
+	  	<div class="hovereffect">
+	    <img class="card-img-top img-responsive" src="pict/4.jpg" alt="Card image" style="width:100%">
+	    <div class="overlay">
+                <h2>HELP</h2>
+				<p>
+					<a href="#">DONATE HERE</a>
+				</p>
+            </div>
+	    </div>
 	    <div class="card-body">
 	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
 	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-	      <a href="#" class="btn btn-primary">See Detail</a>
+	      <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">See Detail</button>
 	    </div>
 	  </div>
 	  </div>
 	  <br>
 
+<!-- Modal content-->
+	<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+     </div>
+    </div>
 
 	 <div class="col-xs-12 col-sm-4">
 	  <div class="card" style="width: 100%;margin-bottom: 10px;">
@@ -124,7 +152,7 @@
 	    <div class="card-body">
 	      <h4 class="card-title">Malam Inspirasi, Semarak Milad Izi</h4>
 	      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-	      <a href="#" class="btn btn-primary">See Detail</a>
+	      <button type="button" class="btn btn-primary">See Detail</button>
 	    </div>
 	  </div>
 	  </div>
@@ -132,6 +160,7 @@
 	</div>
 
 </div>
+
 
 </body>
 </html>
